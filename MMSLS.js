fa17a69bc6d7caf4edc46e05b33ab53dd3539c8b
yxmyxmyyy <yxm1010122195@gmail.com>
2024-05-13 06:33:47 +0800

    Update MMSLS.js

--- MMSLS.js
+++ MMSLS.js
@@ -20,7 +20,7 @@ const MMSLS = [
  */
 const MMSLS = [
     
-    "https://www.maimemo.com/share/page?uid=28925003&pid=0f2b6f2915503958859ac11202339e37&tid=a29e79b3f62abb0d827f71838bf05bea",
+    "https://www.maimemo.com/share/page?uid=28925003&pid=2429673e9c2f2e5521aed9c0465aa8b4&tid=f18baa02dd462ac30744793afd1cf068",
 
 
   ]
